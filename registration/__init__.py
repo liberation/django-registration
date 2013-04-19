91b23819418a8e005237b22d04ce5261fd33beea
Julien Fache <j.fache@liberation.fr>
2013-04-19 16:32:53 +0200

    Apply fix proposed in #41 to remove Django dependancy when installing the package

--- registration/__init__.py
+++ registration/__init__.py
@@ -1,9 +1,20 @@
 VERSION = (0, 9, 0, 'beta', 1)
 
+def get_version(version=VERSION):
+    "Returns a PEP 386-compliant version number from VERSION."
+    assert len(version) == 5
+    assert version[3] in ('alpha', 'beta', 'rc', 'final')
 
-def get_version():
-    try:
-        from django.utils.version import get_version as django_get_version
-    except ImportError:
-        from django import get_version as django_get_version
-    return django_get_version(VERSION) # pragma: no cover
+    # Now build the two parts of the version number:
+    # main = X.Y[.Z]
+    # sub = .devN - for pre-alpha releases
+    #     | {a|b|c}N - for alpha, beta and rc releases
+
+    parts = 2 if version[2] == 0 else 3
+    main = '.'.join(str(x) for x in version[:parts])
+
+    mapping = {'alpha': 'a', 'beta': 'b', 'rc': 'c'}
+    sub = mapping[version[3]] + str(version[4])
+
+    return str(main + sub)
+
